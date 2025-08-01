998bc25f6bb7cdd74db2eef902c2811827e4c8b9
Elieinbar <elieinbar@gmail.com>
2025-08-01 15:26:12 +0300

    ip for windows and mac

--- src/Android/app/src/main/java/com/example/sunmail/api/ApiClient.java
+++ src/Android/app/src/main/java/com/example/sunmail/api/ApiClient.java
@@ -11,7 +11,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "http://192.168.1.230:8080/api/";
+    //windows
+    private static final String BASE_URL = "http://192.168.27.142:8080/api/";
+    //mac
+    // private static final String BASE_URL = "http://192.168.1.230:8080/api/";
     private static Retrofit retrofit;
 
     public static Retrofit get(Context context) {
